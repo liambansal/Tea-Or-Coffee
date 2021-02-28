2c33e9cad422296fb30f80fe25372e0ca117a8a8
liambansal <55756009+liambansal@users.noreply.github.com>
2021-02-28 18:53:24 +0000

    Dispenser Tube
    Created scripts for a button and dispenser
    Dispensing items from tubes
    Removed items spawners & scripts

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Pressable button for VR hand interaction. Execute command on press.
+/// </summary>
+public class Button : MonoBehaviour {
+	[SerializeField]
+	private string itemToSpawn = "";
+
+	/// <summary>
+	/// Tube to spawn an item.
+	/// </summary>
+	[SerializeField]
+	private Dispenser itemTube = null;
+
+	private void Update() {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Debug.Log("Press");
+			itemTube.Dispense(itemToSpawn);
+		}
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Hand")) {
+			Debug.Log("Press");
+			itemTube.Dispense(itemToSpawn);
+		}
+	}
+}
---
+++ Assets/Scripts/Dispenser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Dispenser : MonoBehaviour {
+	[SerializeField]
+	private Transform spawnPoint = null;
+
+	[SerializeField]
+	public GameObject[] items = null;
+
+	private Dictionary<string, GameObject> itemsToSpawn = new Dictionary<string, GameObject>();
+
+	private void Start() {
+		// Add all spawnable items to dictionary.
+		for (int i = 0; i < items.Length; ++i) {
+			itemsToSpawn.Add(items[i].name, items[i]);
+		}
+	}
+
+	/// <summary>
+	/// Spawns an item.
+	/// </summary>
+	public void Dispense(string itemToSpawn) {
+		Instantiate(itemsToSpawn[itemToSpawn], spawnPoint.position, Quaternion.LookRotation(Vector3.up, Vector3.forward));
+	}
+}
